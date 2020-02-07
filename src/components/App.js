4abd4c0ec820e72c04c3caf496690eb3832e26ec
kevinkim1030 <kevin.kim1030@gmail.com>
2020-02-07 11:43:49 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,55 @@
 import React from 'react'
-
 import Filters from './Filters'
 import PetBrowser from './PetBrowser'
 
-class App extends React.Component {
-  constructor() {
-    super()
 
-    this.state = {
-      pets: [],
-      filters: {
-        type: 'all'
-      }
+class App extends React.Component {
+
+  state = {
+    pets: [],
+    filters: {
+      type: 'all',
+      isAdopted: false
     }
   }
 
+
+  onChangeType = (event) => {
+    this.setState({
+      filters:{
+        ...this.state.filters,
+        type: event.target.value
+      }
+    })
+  }
+
+  onFindPetsClick = () => {
+    let type = this.state.filters.type
+    let fetchData = () => {
+      return (type === 'all' ? fetch(`/api/pets`) : fetch(`/api/pets?type=${type}`))
+    }
+    fetchData()
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState({
+          pets: data
+        })
+      })
+  }
+
+  onAdoptPet = (petId) => {
+    let targetPet = this.state.pets.find(pet => pet.id === petId)
+    console.log(petId)
+    console.log(targetPet)
+    targetPet.isAdopted = true
+    this.setState({
+      pets: this.state.pets
+    })
+  }
+  
+  
   render() {
+    console.log(this.state.filters.type)
     return (
       <div className="ui container">
         <header>
@@ -24,10 +58,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} isAdopted={this.state.filters.isAdopted} onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,9 +3,14 @@ import React from 'react'
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
-  render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
-  }
+  
+  render(props) {
+    let displayedPets = this.props.pets.map(pet => <Pet key={pet.id} pet={pet} isAdopted={this.props.isAdopted} onAdoptPet={this.props.onAdoptPet}/>)
+    return (
+    <div className="ui cards">
+      {displayedPets}
+    </div>
+    )}
 }
 
 export default PetBrowser
